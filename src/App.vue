7c6386adb440885ab00ec397ad6911a9b51abbc4
Tatsumi0000 <datsunote0211@gmail.com>
2020-05-25 16:13:44 +0900

    Add

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+  <Header />
 
     <v-content>
       <HelloWorld />
@@ -9,12 +10,14 @@
 
 <script>
 import HelloWorld from '@/components/HelloWorld.vue';
+import Header from '@/components/Header.vue';
 
 export default {
   name: 'App',
 
   components: {
     HelloWorld,
+    Header,
   },
 
   data: () => ({
