988d897a94bd1306703ee763000ad8a00ebc473b
Tatsumi0000 <datsunote0211@gmail.com>
2020-05-27 12:30:25 +0900

    Add: VueRouterを使った画面遷移

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,12 @@
   <v-app>
     <Header />
     <v-content>
-      <Iconpoint />
-      <Sent />
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import Iconpoint from '@/components/Iconpoint.vue';
-import Sent from '@/components/Sent.vue';
 import Header from '@/components/Header.vue';
 
 
@@ -18,9 +15,7 @@ export default {
   name: 'App',
 
   components: {
-    Iconpoint,
     Header,
-    Sent,
   },
 
   data: () => ({
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
-import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import { routes } from './router/index';
 
 Vue.config.productionTip = false;
+// Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  // base: process.env.BASE_URL,
+  routes,
+});
+
 
 new Vue({
   router,
